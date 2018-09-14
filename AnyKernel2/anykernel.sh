0523091d42ce0da35ea1c2a366ac929dc2b49b17
Dencel K Babu <dencelkbabu007@gmail.com>
2018-09-14 07:58:32 +0530

    AnyKernel2: Enable Module Flashing

--- AnyKernel2/anykernel.sh
+++ AnyKernel2/anykernel.sh
@@ -6,7 +6,7 @@
 properties() { '
 kernel.string=Phantom Kernel by @Dencel007. Credits: TeamQuantum & TeamGhost
 do.devicecheck=1
-do.modules=0
+do.modules=1
 do.cleanup=1
 do.cleanuponabort=1
 device.name1=santoni
